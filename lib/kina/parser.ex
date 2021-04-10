1428b1bc8775afe5b9600833122d3fd1753ba560
insprac <mail@aydan.io>
2021-04-10 12:46:11 +1200

    Added schema and parser

---
+++ lib/kina/parser.ex
@@ -0,0 +1,42 @@
+defmodule Kina.Parser do
+  @spec parse(any, Kina.type()) :: any
+  def parse(nil, _type), do: nil
+
+  def parse(value, :integer) when is_integer(value), do: value
+  def parse(value, :integer), do: value_error(value, :integer)
+  def parse(value, :float) when is_float(value), do: value
+  def parse(value, :float) when is_integer(value), do: value / 1
+  def parse(value, :float), do: value_error(value, :float)
+  def parse(value, :string) when is_binary(value), do: value
+  def parse(value, :string), do: value_error(value, :string)
+
+  def parse(value, type) do
+    cond do
+      not is_atom(type) ->
+        type_error(type)
+
+      Kina.Schema.kina_schema?(type) ->
+        parse_schema(value, type)
+
+      true ->
+        type_error(type)
+    end
+  end
+
+  @spec parse_schema(any, atom) :: struct
+  def parse_schema(value, module) do
+    module.__fields__()
+    |> Enum.reduce(%module{}, fn {name, type, _opts}, schema ->
+      sub_value = Map.get(value, name) || Map.get(value, "#{name}")
+      Map.put(schema, name, parse(sub_value, type))
+    end)
+  end
+
+  defp value_error(value, type) do
+    raise "Invalid value, must be type #{type}: #{inspect(value)}"
+  end
+
+  defp type_error(type) do
+    raise "Invalid type: #{inspect(type)}"
+  end
+end
---
+++ lib/kina/schema.ex
@@ -0,0 +1,46 @@
+defmodule Kina.Schema do
+  @type type :: atom | {atom, type}
+  @type field :: {atom, type, Keyword.t()}
+
+  defmacro __using__(_) do
+    quote do
+      import Kina.Schema
+    end
+  end
+
+  defmacro schema(block) do
+    quote do
+      Module.register_attribute(__MODULE__, :fields, accumulate: true)
+
+      unquote(block)
+
+      defstruct Enum.map(@fields, &elem(&1, 0))
+
+      @spec __kina_schema__() :: true
+      def __kina_schema__, do: true
+
+      @spec __fields__() :: [Kina.Schema.field()]
+      def __fields__, do: @fields
+    end
+  end
+
+  defmacro field(name, type, opts \\ []) do
+    quote do
+      Module.put_attribute(
+        __MODULE__,
+        :fields,
+        {unquote(name), unquote(type), unquote(opts)}
+      )
+    end
+  end
+
+  @spec kina_schema?(atom) :: boolean
+  def kina_schema?(module) do
+    function_exported?(module, :__kina_schema__, 0)
+  end
+
+  @spec parse(map, atom) :: struct
+  def parse(data, schema) do
+    Kina.Parser.parse(data, schema)
+  end
+end
